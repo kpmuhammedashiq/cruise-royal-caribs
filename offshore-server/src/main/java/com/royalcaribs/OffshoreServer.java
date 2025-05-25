25a1eb51339b78458f025273d44ffdf1099b0c85
kpmuhammedashiq <kpmuhammedashiq@gmail.com>
2025-05-25 20:31:29 +0530

    feat: offshore server; proxy receiver and external connection establishing

--- offshore-server/src/main/java/com/royalcaribs/OffshoreServer.java
+++ offshore-server/src/main/java/com/royalcaribs/OffshoreServer.java
@@ -1,32 +1,53 @@
 package com.royalcaribs;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+import java.io.*;
+import java.net.HttpURLConnection;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
 
 public class OffshoreServer {
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(8082);
-        Socket clientSocket = serverSocket.accept();
         System.out.println("Offshore Server - up and running:"+8082);
-
-        BufferedReader bRead = new BufferedReader(new InputStreamReader(clientSocket.getInputStream(), StandardCharsets.UTF_8));
-            while (true) {
-                String data;
-                StringBuilder request = new StringBuilder();
-                while ((data = bRead.readLine()) != null  && !data.isEmpty()) {
-                    request.append(data).append("\r\n");
-                }
-
-                System.out.println("Received request:" + request);
-
-//                clientSocket.close();
-//                serverSocket.close();
+        Socket clientSocket = serverSocket.accept();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream(), StandardCharsets.UTF_8));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream(), StandardCharsets.UTF_8));
+        while (true) {
+            String data;
+            StringBuilder request = new StringBuilder();
+            while ((data = bufferedReader.readLine()) != null  && !data.isEmpty()) {
+                request.append(data).append("\r\n");
             }
+            System.out.println("Request: " + request);
+
+            String[] requestParts = request.toString().split("\r\n")[0].split(" ");
+            String method = requestParts[0];
+            String reqUrl = requestParts[1];
+            OkHttpClient client = new OkHttpClient();
+            RequestBody requestBody = null;
+            if ("POST".equalsIgnoreCase(method) || "PUT".equalsIgnoreCase(method)) {
+                requestBody = RequestBody.create("", null);
+            }
+            Request rqExternal = new Request.Builder()
+                    .url(reqUrl)
+                    .method(method, requestBody)
+                    .build();
+            Response rpExternal = client.newCall(rqExternal).execute();
+            String rpExternalData = rpExternal.body().string();
+//            System.out.println(rpExternalData);
+            bufferedWriter.write("HTTP/1.1 200 OK\r\n");
+            bufferedWriter.write("Content-Type: text/plain\r\n\r\n");
+            bufferedWriter.write(rpExternalData);
+            bufferedWriter.flush();
+        }
 
     }
 }
